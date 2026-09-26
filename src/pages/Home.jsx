3150eddcc047ebd522ade0c8e16131ab4728262f
AVIEK ROY <theaviekroy@gmail.com>
2026-09-26 20:36:51 +0530

    Show logged in user name on home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,27 +1,40 @@
 import Hero from "../components/Hero";
-// import Features from "../components/Features";
 import Category from "../components/Category";
 import ProductCard from "../components/ProductCard";
 import Ratings from "../components/Ratings";
 import Footer from "../components/Footer";
-import userIcon4 from "../assets/img/image (4).png";
 
 const Home = ({ products }) => {
+
+  const user = JSON.parse(localStorage.getItem("user"));
+
+  const userName =
+    user?.name ||
+    user?.displayName ||
+    user?.email?.split("@")[0] ||
+    "User";
+
   return (
     <div>
+
       <div className="p-5">
-        <Hero />
+        <Hero userName={userName} />
       </div>
-<div>
-  <ProductCard />
-</div>
+
+      <div>
+        <ProductCard />
+      </div>
+
       <div className="p-5">
         <Category products={products} />
       </div>
+
       <div className="p-4">
         <Ratings />
       </div>
+
       <Footer />
+
     </div>
   );
 };
