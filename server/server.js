466ea8c44d741fd46bad9de0fcc9f9b41616fc7b
AVIEK ROY <theaviekroy@gmail.com>
2026-09-26 19:40:20 +0530

    Fix Aiven MySQL connection

--- server/server.js
+++ server/server.js
@@ -16,7 +16,11 @@ const db = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    port: Number(process.env.DB_PORT),
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 // Connect to MySQL
